31fbc3c4d3dfa165e6201e37af5660904990eb4d
cyty2002 <cyty2002@gmail.com>
2021-03-05 00:06:27 +0800

    add proj

---
+++ cmake-build-debug/CMakeFiles/clionPG.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clionPG.dir/main.cpp.o"
+  "clionPG"
+  "clionPG.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clionPG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
